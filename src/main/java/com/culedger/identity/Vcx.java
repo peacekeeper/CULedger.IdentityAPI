50f1856dcf7a04e826156a5175bae5e82daad3ac
Markus Sabadello <markus@danubetech.com>
2018-12-26 10:32:55 +0100

    Don't re-create schemas and creddefs.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- src/main/java/com/culedger/identity/Vcx.java
+++ src/main/java/com/culedger/identity/Vcx.java
@@ -1,6 +1,15 @@
 package com.culedger.identity;
+import java.io.File;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.hyperledger.indy.sdk.IndyException;
+import org.hyperledger.indy.sdk.LibIndy;
+import org.hyperledger.indy.sdk.ledger.Ledger;
+import org.hyperledger.indy.sdk.pool.Pool;
+import org.hyperledger.indy.sdk.pool.PoolJSONParameters.OpenPoolLedgerJSONParameter;
+import org.hyperledger.indy.sdk.wallet.Wallet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,6 +19,7 @@ import com.culedger.identity.didmapper.DbMemberDidMapper;
 import com.culedger.identity.didmapper.MemberDidMapper;
 import com.culedger.identity.didmapper.MemoryMemberDidMapper;
 import com.evernym.sdk.vcx.LibVcx;
+import com.evernym.sdk.vcx.VcxException;
 import com.evernym.sdk.vcx.connection.ConnectionApi;
 import com.evernym.sdk.vcx.credentialDef.CredentialDefApi;
 import com.evernym.sdk.vcx.issuer.IssuerApi;
@@ -42,10 +52,18 @@ public class Vcx {
 	public static final int PROOFSTATE_VERIFIED = 1;
 	public static final int PROOFSTATE_INVALID = 2;
 
+	private static final String POOL_NAME = "pool1";
+	private static final String WALLET_CONFIG = "{ \"id\":\"" + "culedger" + "\", \"storage_type\":\"" + "default" + "\"}";
+	private static final String WALLET_CREDENTIALS = "{ \"key\":\"culedger-key\" }";
+	private static final String SCHEMA_DATA = "[\"cu\"]";
+
+	private static Pool pool;
+	private static Wallet wallet;
+
 	private static MemberDidMapper memberDidMapper;
 
 	private static String schemaId;
-	private static String credentialDefId;
+	private static String credDefId;
 	private static Integer schemaSeqNo;
 
 	static {
@@ -61,58 +79,155 @@ public class Vcx {
 
 		try {
 
-			// init
+			VcxConfiguration.init();
 
-			LibVcx.init();
-			Integer init = VcxApi.vcxInit("/opt/sovrin/vcxconfig.json").get();
-			if (init == null || init.intValue() != 0) throw new RuntimeException("VCX init failed.");
+			// init indy
 
-			if (logger.isInfoEnabled()) logger.info("VCX initialized.");
+			initLibIndy();
 
-			// create member->DID mapper
+			Boolean schemaExists = checkSchemaExists(VcxConfiguration.VCX_SCHEMA_ID);
+			if (logger.isInfoEnabled()) logger.info("Schema exists? " + schemaExists);
 
-			if (VcxConfiguration.VCX_DID_MAPPER.equals("memory")) {
+			Boolean credDefExists = checkCredDefExists(VcxConfiguration.VCX_CREDDEF_ID);
+			if (logger.isInfoEnabled()) logger.info("CredDef exists? " + credDefExists);
 
-				memberDidMapper = new MemoryMemberDidMapper();
-			} else if (VcxConfiguration.VCX_DID_MAPPER.equals("db")) {
+			closeLibIndy();
 
-				memberDidMapper = new DbMemberDidMapper();
-			} else {
+			// init vcx
 
-				throw new Exception("Unknown DID->mapper type.");
-			}
+			initLibVcx();
 
-			// create schema
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 
-			String sourceId = VcxConfiguration.VCX_INSTITUTION_DID;
-			String schemaName = "mycuid-" +  + random.nextInt(1000000);
-			String version = "0.0.1";
-			String schemaData = "[\"cu\"]";
-			Integer schemaHandle = SchemaApi.schemaCreate(sourceId, schemaName, version, schemaData, 0).get();
-			schemaId = SchemaApi.schemaGetSchemaId(schemaHandle).get();
-			String schema = SchemaApi.schemaSerialize(schemaHandle).get();
-			schemaSeqNo = 0;
+				try {
 
-			if (logger.isInfoEnabled()) logger.info("For source ID " + sourceId + " got schema handle " + schemaHandle + " and schema ID " + schemaId + " and schema " + schema + " and schema seq no " + schemaSeqNo);
+					closeLibVcx();
+				} catch (VcxException ex) {
 
-			// create credential definition
+					throw new RuntimeException(ex.getMessage(), ex);
+				}
+			}));
 
-			String issuerId = sourceId;
-			String credentialName = "culedger-" + random.nextInt(1000000);
-			String tag = "test." + random.nextInt(1000000);
-			String config = "{\"support_revocation\":false}";
-			Integer credentialDefHandle = CredentialDefApi.credentialDefCreate(sourceId, credentialName, schemaId, issuerId, tag, config, 0).get();
-			credentialDefId = CredentialDefApi.credentialDefGetCredentialDefId(credentialDefHandle).get();
-			String credentialDef = CredentialDefApi.credentialDefSerialize(credentialDefHandle).get();
+			// create schema and creddef
 
-			if (logger.isInfoEnabled()) logger.info("For issuer ID " + issuerId + " got credential def handle " + credentialDefHandle + " and credential def ID " + credentialDefId + " and credential def " + credentialDef);
+			if (VcxConfiguration.VCX_SCHEMA_ID != null) schemaId = VcxConfiguration.VCX_SCHEMA_ID; else schemaId = initCreateSchema(); 
+			if (VcxConfiguration.VCX_CREDDEF_ID != null) credDefId = VcxConfiguration.VCX_CREDDEF_ID; else credDefId = initCreateCredDef(); 
 		} catch (Exception ex) {
 
 			if (logger.isErrorEnabled()) logger.error(ex.getMessage(), ex);
 			schemaId = null;
-			credentialDefId = null;
+			credDefId = null;
 			System.exit(-1);
 		}
+	}
+
+	private static void initLibIndy() throws IndyException, InterruptedException, ExecutionException {
+
+		if (! LibIndy.isInitialized()) LibIndy.init(new File("./lib/libindy.so"));
+		Pool.setProtocolVersion(2);
+
+		pool = Pool.openPoolLedger(POOL_NAME, new OpenPoolLedgerJSONParameter(null, null).toJson()).get();
+		if (logger.isInfoEnabled()) logger.info("pool: " + pool);
+
+		wallet = Wallet.openWallet(WALLET_CONFIG, WALLET_CREDENTIALS).get();
+		if (logger.isInfoEnabled()) logger.info("wallet: " + wallet);
+	}
+
+	private static void initLibVcx() throws InterruptedException, ExecutionException, VcxException {
+
+		LibVcx.init();
+		Integer init = VcxApi.vcxInit("/opt/sovrin/vcxconfig.json").get();
+		if (init == null || init.intValue() != 0) throw new RuntimeException("VCX init failed.");
+
+		if (logger.isInfoEnabled()) logger.info("VCX initialized.");
+
+		// create member->DID mapper
+
+		if (VcxConfiguration.VCX_DID_MAPPER.equals("memory")) {
+
+			memberDidMapper = new MemoryMemberDidMapper();
+		} else if (VcxConfiguration.VCX_DID_MAPPER.equals("db")) {
+
+			memberDidMapper = new DbMemberDidMapper();
+		} else {
+
+			throw new IllegalArgumentException("Unknown DID->mapper type.");
+		}
+	}
+
+	private static void closeLibIndy() throws InterruptedException, ExecutionException, IndyException {
+
+		wallet.closeWallet().get();
+		pool.closePoolLedger().get();
+
+		if (logger.isInfoEnabled()) logger.info("LibIndy closed");
+	}
+
+	private static void closeLibVcx() throws VcxException {
+
+		VcxApi.vcxShutdown(Boolean.FALSE);
+
+		if (logger.isInfoEnabled()) logger.info("LibVcx closed");
+	}
+
+	private static Boolean checkSchemaExists(String schemaId) throws IndyException, InterruptedException, ExecutionException {
+
+		if (schemaId == null) return null;
+
+		String submitterDid = VcxConfiguration.VCX_INSTITUTION_DID;
+
+		String getSchemaRequestJson = Ledger.buildGetSchemaRequest(submitterDid, schemaId).get();
+		if (logger.isInfoEnabled()) logger.info("getSchemaRequestJson: " + getSchemaRequestJson);
+		String getSchemaRequestResult = Ledger.signAndSubmitRequest(pool, wallet, submitterDid, getSchemaRequestJson).get();
+		if (logger.isInfoEnabled()) logger.info("getSchemaRequestResult: " + getSchemaRequestResult);
+
+		return Boolean.valueOf(getSchemaRequestResult != null);
+	}
+
+	private static Boolean checkCredDefExists(String credDefId) throws InterruptedException, ExecutionException, IndyException {
+
+		if (credDefId == null) return null;
+
+		String submitterDid = VcxConfiguration.VCX_INSTITUTION_DID;
+
+		String getCredDefRequestJson = Ledger.buildGetCredDefRequest(submitterDid, credDefId).get();
+		if (logger.isInfoEnabled()) logger.info("getCredDefRequestJson: " + getCredDefRequestJson);
+		String getCredDefRequestResult = Ledger.signAndSubmitRequest(pool, wallet, submitterDid, getCredDefRequestJson).get();
+		if (logger.isInfoEnabled()) logger.info("getCredDefRequestResult: " + getCredDefRequestResult);
+
+		return Boolean.valueOf(getCredDefRequestResult != null);
+	}
+
+	private static String initCreateSchema() throws InterruptedException, ExecutionException, VcxException {
+
+		String sourceId = VcxConfiguration.VCX_INSTITUTION_DID;
+		String schemaName = schemaNameFromSchemaData(SCHEMA_DATA);
+		String version = schemaVersionFromSchemaData(SCHEMA_DATA);
+		String schemaData = SCHEMA_DATA;
+		Integer schemaHandle = SchemaApi.schemaCreate(sourceId, schemaName, version, schemaData, 0).get();
+		String schemaId = SchemaApi.schemaGetSchemaId(schemaHandle).get();
+		String schema = SchemaApi.schemaSerialize(schemaHandle).get();
+		schemaSeqNo = 0;
+
+		if (logger.isInfoEnabled()) logger.info("For source ID " + sourceId + " got schema handle " + schemaHandle + " and schema ID " + schemaId + " and schema " + schema + " and schema seq no " + schemaSeqNo);
+
+		return schemaId;
+	}
+
+	private static String initCreateCredDef() throws InterruptedException, ExecutionException, VcxException {
+
+		String sourceId = VcxConfiguration.VCX_INSTITUTION_DID;
+		String issuerId = VcxConfiguration.VCX_INSTITUTION_DID;
+		String credDefName = credDefName();
+		String credDefTag = credDefTag();
+		String config = "{\"support_revocation\":false}";
+		Integer credentialDefHandle = CredentialDefApi.credentialDefCreate(sourceId, credDefName, schemaId, issuerId, credDefTag, config, 0).get();
+		String credDefId = CredentialDefApi.credentialDefGetCredentialDefId(credentialDefHandle).get();
+		String credDef = CredentialDefApi.credentialDefSerialize(credentialDefHandle).get();
+
+		if (logger.isInfoEnabled()) logger.info("For issuer ID " + issuerId + " got creddef handle " + credentialDefHandle + " and creddef ID " + credDefId + " and creddef " + credDef);
+
+		return credDefId;
 	}
 
 	public static ResponseEntity<CULedgerOnboardingData> memberOnBoard(String memberId, CULedgerOnboardingData cuLedgerOnboardingData) {
@@ -186,9 +301,9 @@ public class Vcx {
 
 			String credentialData = credentialData(VcxConfiguration.VCX_INSTITUTION_NAME);
 			String credentialName = VcxConfiguration.VCX_CREDENTIAL_NAME;
-			Integer credentialHandle = IssuerApi.issuerCreateCredential(sourceId, credentialDefId, null, credentialData, credentialName, "0").get();
+			Integer credentialHandle = IssuerApi.issuerCreateCredential(sourceId, credDefId, null, credentialData, credentialName, "0").get();
 
-			if (logger.isInfoEnabled()) logger.info("For source ID " + sourceId + " and credential def ID " + credentialDefId + " and credential data " + credentialData + " got credential handle " + credentialHandle);
+			if (logger.isInfoEnabled()) logger.info("For source ID " + sourceId + " and credential def ID " + credDefId + " and credential data " + credentialData + " got credential handle " + credentialHandle);
 
 			// send credential offer
 
@@ -355,6 +470,37 @@ public class Vcx {
 		return requestedAttrs.replace("'", "\"");
 	}
 
+	private static String schemaNameFromSchemaData(String schemaData) {
+
+		return "mycuid-" + random.nextInt(1000000);
+		//		return "mycuid-" + DigestUtils.sha256Hex(schemaData);
+	}
+
+	private static String schemaVersionFromSchemaData(String schemaData) {
+
+		return "0.0.1";
+	}
+
+	private static String credDefName() {
+
+		return "culedger-" + random.nextInt(1000000);
+	}
+
+	private static String credDefTag() {
+
+		return "tag-" + random.nextInt(1000000);
+	}
+/*
+	private static String schemaIdFromSchemaData(String schemaData) {
+
+		return VcxConfiguration.VCX_INSTITUTION_DID + ":" + "2" + ":" + schemaNameFromSchemaData(schemaData) + ":" + schemaVersionFromSchemaData(schemaData);
+	}
+
+	private static String credDefIdFromSchemaData(String schemaData) {
+
+		return null;
+	}*/
+
 	private static String credentialOffer(String toDid, String fromDid, String cuValue) {
 
 		String credentialOffer = 
@@ -370,7 +516,7 @@ public class Vcx {
 						"    ]\n" +
 						"  },\n" +
 						"  'schema_seq_no': " + schemaSeqNo + ",\n" +
-						"  'cred_def_id': '" + credentialDefId + "',\n" +
+						"  'cred_def_id': '" + credDefId + "',\n" +
 						"  'claim_name': 'Credential',\n" +
 						"  'claim_id': 'defaultCredentialId',\n" +
 						"  'msg_ref_id': ''\n" +
--- src/main/java/com/culedger/identity/VcxConfiguration.java
+++ src/main/java/com/culedger/identity/VcxConfiguration.java
@@ -11,23 +11,43 @@ public class VcxConfiguration {
 	public static final String VCX_INSTITUTION_DID;
 	public static final String VCX_CREDENTIAL_NAME;
 	public static final String VCX_DID_MAPPER;
+	public static final String VCX_SCHEMA_ID;
+	public static final String VCX_CREDDEF_ID;
 
 	static {
 
-		VCX_INSTITUTION_NAME = System.getenv("VCX_INSTITUTION_NAME");
-		if (VCX_INSTITUTION_NAME == null) throw new RuntimeException("Variable VCX_INSTITUTION_NAME not set.");
+		String E_VCX_INSTITUTION_NAME = System.getenv("VCX_INSTITUTION_NAME");
+		if (E_VCX_INSTITUTION_NAME == null) throw new RuntimeException("Variable VCX_INSTITUTION_NAME not set.");
+		VCX_INSTITUTION_NAME = E_VCX_INSTITUTION_NAME;
 		if (logger.isInfoEnabled()) logger.info("VCX_INSTITUTION_NAME: " + VCX_INSTITUTION_NAME);
 
-		VCX_INSTITUTION_DID = System.getenv("VCX_INSTITUTION_DID");
-		if (VCX_INSTITUTION_DID == null) throw new RuntimeException("Variable VCX_INSTITUTION_DID not set.");
+		String E_VCX_INSTITUTION_DID = System.getenv("VCX_INSTITUTION_DID");
+		if (E_VCX_INSTITUTION_DID == null) throw new RuntimeException("Variable VCX_INSTITUTION_DID not set.");
+		VCX_INSTITUTION_DID = E_VCX_INSTITUTION_DID;
 		if (logger.isInfoEnabled()) logger.info("VCX_INSTITUTION_DID: " + VCX_INSTITUTION_DID);
 
-		VCX_CREDENTIAL_NAME = System.getenv("VCX_CREDENTIAL_NAME");
-		if (VCX_CREDENTIAL_NAME == null) throw new RuntimeException("Variable VCX_CREDENTIAL_NAME not set.");
+		String E_VCX_CREDENTIAL_NAME = System.getenv("VCX_CREDENTIAL_NAME");
+		if (E_VCX_CREDENTIAL_NAME == null) throw new RuntimeException("Variable VCX_CREDENTIAL_NAME not set.");
+		VCX_CREDENTIAL_NAME = E_VCX_CREDENTIAL_NAME;
 		if (logger.isInfoEnabled()) logger.info("VCX_CREDENTIAL_NAME: " + VCX_CREDENTIAL_NAME);
 
-		VCX_DID_MAPPER = System.getenv("VCX_DID_MAPPER");
-		if (VCX_DID_MAPPER == null) throw new RuntimeException("Variable VCX_DID_MAPPER not set.");
+		String E_VCX_DID_MAPPER = System.getenv("VCX_DID_MAPPER");
+		if (E_VCX_DID_MAPPER == null) throw new RuntimeException("Variable VCX_DID_MAPPER not set.");
+		VCX_DID_MAPPER = E_VCX_DID_MAPPER;
 		if (logger.isInfoEnabled()) logger.info("VCX_DID_MAPPER: " + VCX_DID_MAPPER);
+
+		String E_VCX_SCHEMA_ID = System.getenv("VCX_SCHEMA_ID");
+		if (E_VCX_SCHEMA_ID != null && E_VCX_SCHEMA_ID.trim().isEmpty()) E_VCX_SCHEMA_ID = null;
+		VCX_SCHEMA_ID = E_VCX_SCHEMA_ID;
+		if (logger.isInfoEnabled()) logger.info("VCX_SCHEMA_ID: " + VCX_SCHEMA_ID);
+
+		String E_VCX_CREDDEF_ID = System.getenv("VCX_CREDDEF_ID");
+		if (E_VCX_CREDDEF_ID != null && E_VCX_CREDDEF_ID.trim().isEmpty()) E_VCX_CREDDEF_ID = null;
+		VCX_CREDDEF_ID = E_VCX_CREDDEF_ID;
+		if (logger.isInfoEnabled()) logger.info("VCX_CREDDEF_ID: " + VCX_CREDDEF_ID);
+	}
+
+	public static void init() {
+
 	}
 }
