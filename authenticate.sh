7247e213a13acff924766f4ebad22806571059ba
Markus Sabadello <markus@danubetech.com>
2019-03-22 10:12:35 +0100

    Update scripts to match 0.2.0 Swagger API definition.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- authenticate.sh
+++ authenticate.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type: application/json" -X PUT http://localhost:8080/darrellodonnell/CULedger.Identity/0.1.0/member/ms7827/authenticate
+curl -H "Content-Type: application/json" -X PUT http://localhost:8080/CULedger/CULedger.Identity/0.2.0/member/ms7827/authenticate
--- onboard.sh
+++ onboard.sh
@@ -1,19 +1,18 @@
 input_json=$(cat <<EOF
 {
     "memberId": "ms7827",
-    "memberPhoneNumber": "+17703299814",
-    "memberEmail": "tshelton@culedger.com",
+    "phoneNumber": "+17703299814",
+    "emailAddress": "tshelton@culedger.com",
     "displayTextFromFI": "Let's get connected via MyCUID!",
     "credentialData": {
-        "id": "UUID-GOES-HERE",
+        "CredentialId": "UUID-GOES-HERE",
         "Institution": "CULedger Credit Union",
-        "memberId": "ms7827",
-        "status": "active",
-        "memberSince": null,
-        "credDataPayload": ""
+        "Credential": null,
+        "MemberNumber": "ms7827",
+        "MemberSince": null
     }
 }
 EOF
 )
 
-curl -H "Content-Type: application/json" -X POST -d "$input_json" http://localhost:8080/darrellodonnell/CULedger.Identity/0.1.0/member/ms7827/onboard
+curl -H "Content-Type: application/json" -X POST -d "$input_json" http://localhost:8080/CULedger/CULedger.Identity/0.2.0/member/ms7827/onboard
